141989a03dc70cd43991d82884b68c3d88110b7f
rxy20500 <rxy20300@gmail.com>
2026-06-03 01:17:47 +0200

    Add files via upload

---
+++ HabitosApp/Domain/Entities/TemaForo.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace HabitosApp.Domain.Entities
+{
+    public class TemaForo
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+        public string Categoria { get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+        
+        // Relación con el usuario que lo crea
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+        // Relación con los comentarios
+        public List<RespuestaForo> Respuestas { get; set; } = new List<RespuestaForo>();
+    }
+}
