406dff4e94c6289b55a1e6e8b2384d5ae703f3c5
rxy20500 <rxy20300@gmail.com>
2026-06-03 01:35:15 +0200

    Make TemaForo and Usuario properties nullable
    Added nullable types for TemaForo and Usuario properties.

--- HabitosApp/Domain/Entities/RespuestaForo.cs
+++ HabitosApp/Domain/Entities/RespuestaForo.cs
@@ -8,10 +8,12 @@ namespace HabitosApp.Domain.Entities
         public string Contenido { get; set; }
         public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
 
+        // Relación con el tema (Opcional en la validación de entrada)
         public int TemaForoId { get; set; }
-        public TemaForo TemaForo { get; set; }
+        public TemaForo? TemaForo { get; set; }
 
+        // Relación con el usuario (Opcional en la validación de entrada)
         public int UsuarioId { get; set; }
-        public Usuario Usuario { get; set; }
+        public Usuario? Usuario { get; set; }
     }
 }
